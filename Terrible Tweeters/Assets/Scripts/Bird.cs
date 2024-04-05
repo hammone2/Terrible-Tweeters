79381d9f64142c365af0148921ef75ad1d4d8ca2
hammone2 <hammone2@my.erau.edu>
2024-04-04 19:40:29 -0700

    added stuff

--- Terrible Tweeters/Assets/Scripts/Bird.cs
+++ Terrible Tweeters/Assets/Scripts/Bird.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class Bird : MonoBehaviour
 {
     [SerializeField] float _launchForce = 500;
+    [SerializeField] float _maxDragDistance = 3.5f;
 
     Vector2 _startPosition;
     Rigidbody2D _rigidbody2D;
     SpriteRenderer _spriteRenderer;
+    bool _resetting;
+
+    public bool IsDragging { get; private set; }
 
 
     void Awake()
@@ -27,6 +31,7 @@ public class Bird : MonoBehaviour
     void OnMouseDown()
     {
         _spriteRenderer.color = Color.red;
+        IsDragging = true;
     }
 
     void OnMouseUp()
@@ -39,17 +44,46 @@ public class Bird : MonoBehaviour
         _rigidbody2D.AddForce(direction * _launchForce);
 
         _spriteRenderer.color = Color.white;
+        IsDragging = false;
     }
 
     void OnMouseDrag()
     {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = new Vector3(mousePosition.x, mousePosition.y, transform.position.z);
+
+        Vector2 desiredPosition = mousePosition;
+        
+
+        float distance = Vector2.Distance(desiredPosition, _startPosition);
+        if (distance > _maxDragDistance)
+        {
+            Vector2 direction = desiredPosition - _startPosition;
+            direction.Normalize();
+            desiredPosition = _startPosition + (direction * _maxDragDistance);
+        }
+
+        if (desiredPosition.x > _startPosition.x)
+            desiredPosition.x = _startPosition.x;
+
+        _rigidbody2D.position = desiredPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        StartCoroutine(ResetAfterDelay());
+        
+    }
+
+    IEnumerator ResetAfterDelay()
+    {
+        yield return new WaitForSeconds(3);
+        _rigidbody2D.position = _startPosition;
+        _rigidbody2D.isKinematic = true;
+        _rigidbody2D.velocity = Vector2.zero;
     }
 }
